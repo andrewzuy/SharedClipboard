4dfa9e582dbfa38681d291c4492a13aac069e63f
andrewzuy <andrewzuy@gmail.com>
2024-01-01 20:42:55 +0300

    added TUI interface

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,4 @@
+use std::sync::mpsc::{channel, Sender, Receiver};
 use std::io::{stdin};
 use std::sync::{Arc, Mutex};
 extern crate clipboard;
@@ -8,9 +9,19 @@ use serde::{Deserialize};
 use reqwest::blocking::Client;
 mod aes_encryption;
 use std::fs::File;
-use std::io::Write;
 use std::path::Path;
 use crate::aes_encryption::aes_encryption::{AesEngine, Message, FileMessage};
+use std::io::{stdout, Write};
+use std::time::{Duration};
+use crossterm::event::{KeyEventKind, EnableBracketedPaste};
+use crossterm::{
+    ExecutableCommand, QueueableCommand,
+    execute,
+    cursor,
+    terminal::{self, enable_raw_mode, disable_raw_mode},
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor, PrintStyledContent},
+    event::{poll, read, Event, KeyEvent, KeyCode, KeyModifiers, ModifierKeyCode},
+};
 
 #[derive(Debug,Deserialize, Clone)]
 struct Config{
@@ -19,55 +30,174 @@ struct Config{
     Passkey:String
 }
 
-fn tui_scan(config:Arc<Mutex<Config>>, client:Arc<Client>){
-    let mut temp = String::new();
+fn render_ui(config:Arc<Mutex<Config>>, client:Arc<Client>, log:Arc<Mutex<Vec<String>>>){
     let key = aes_encryption::aes_encryption::get_key(config.clone().lock().unwrap().clone().Passkey);
-    loop {
-        print!("{}[2J", 27 as char);
-        println!("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
-        println!("┃Enter F to send a file, R to receive...┃");
-        println!("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
-        let _ = stdin().read_line(&mut temp);
-        if temp.contains("F") {
-            print!("{}[2J", 27 as char);          
-            println!("┏━━━━━━━━━━━━━━━━━━━━━━━━━┓");
-            println!("┃Copy the filepath here...┃");
-            println!("┗━━━━━━━━━━━━━━━━━━━━━━━━━┛");
-            temp.clear();
-            _ = stdin().read_line(&mut temp);
-	    let file_str = clear_up_path(temp.clone());
+    let mut stdout = stdout();
+    let (mut width, mut heights) = terminal::size().unwrap();
+    let mut filename = String::new();
+    let mut  quit = false;
+    _ = enable_raw_mode();
+    execute!(stdout, EnableBracketedPaste).unwrap();
+    _= stdout.execute(terminal::Clear(terminal::ClearType::All));
+    while !quit{
+	if  poll(Duration::from_millis(300 as u64)).unwrap(){
+	    match read(){
+		Ok(ev) => match ev{
+		    Event::Resize(w, h) => { width = w; heights = h},
+		    Event::Key(key_event) => {
+			match key_event.code{
+			    KeyCode::Char(x) => {
+				if x == 'c' && key_event.modifiers.contains(KeyModifiers::CONTROL){
+				     quit = true;
+				}
+				if x  == 'r' && key_event.modifiers.contains(KeyModifiers::CONTROL){
+				    handle_receive_file(config.clone(), client.clone(), key.clone(), log.clone());
+				}
+			    },
+			_ => ()
+			}
+		    },
+		    Event::Paste(path) => {
+			filename.clear();
+			filename.push_str(&path);
+			match log.try_lock(){
+			    Ok(mut res) =>{
+				res.push(format!("Sending file: {}",filename.clone()))
+			    },
+			    Err(_) => ()
+			}
+			handle_sending_file(filename.clone(), config.clone(), client.clone(), key.clone(), log.clone());
+		    }
+		    _ => ()
+		},
+		Err(_) => ()
+	    }   
+	}
+	render_rectangle(width, heights, 0, 0);
+	render_header(width-1,heights,1,1,"Secure Clipboard",'░');
+	if heights > 4{
+	    render_header(width-1, heights,1,heights/2,"Drag and drop a file to send, CTRL + R to receive, CTRL + Q to quit",'═');
+	    render_header(width-1,heights,1,heights*3/4,"Log:",'░');
+	} if heights > 9{
+	    match log.try_lock(){
+		Ok(mut res) =>{
+		    let temp = String::new();
+		    render_text(1, heights*3/4 +1, &res.last().unwrap_or_else(||{&temp}));
+		    if res.len() > 2 {
+			render_text(1, heights*3/4 +2, &res.get(res.len()-2).unwrap_or_else(||{&temp}));
+			render_text(1, heights*3/4 +3, &res.get(res.len()-3).unwrap_or_else(||{&temp}));
+		    }
+		},
+		Err(_) => ()
+	}
+
+	}
+	
+	stdout.flush();
+	thread::sleep(Duration::from_millis(100 as u64));
+    }
+    
+    _ = disable_raw_mode();
+    _= stdout.execute(terminal::Clear(terminal::ClearType::All));
+    
+}
+
+fn handle_sending_file(filename:String, config:Arc<Mutex<Config>>, client:Arc<Client>, key:[u8;32], log:Arc<Mutex<Vec<String>>>){
+	    let file_str = clear_up_path(filename.clone());
 	    let file_path = Path::new(&file_str);
-	    println!("File path: {:?}, does exists: {:?}", file_path.file_name(), file_path.exists());
 	    let file_open = std::fs::read(file_path);
 	    match file_open{
 		Ok(file) => {
 		    let file_message = FileMessage::new(file_path.file_name().unwrap().to_str().unwrap().to_string(), file);
-		    post_file(config.clone(), file_message.file.encrypt_aes_256_cbc(&key), file_message.filename.clone(), client.clone())
+		    post_file(config.clone(), file_message.file.encrypt_aes_256_cbc(&key), file_message.filename.clone(), client.clone(), log.clone());
+			match log.try_lock(){
+			    Ok(mut res) => res.push(format!("File sent: {}", filename.clone())),
+			    Err(_) => ()
+			}
 		},
-		Err(err) => eprintln!("Failed to post the file because of: {}", err)
+		Err(err) => match log.try_lock(){
+			    Ok(mut res) => res.push(format!("Failed to post the file because of: {}", err)),
+			    Err(_) => ()
+			}
 	    }
-	    println!("Hit enter...");
-	    _ = stdin().read_line(&mut temp);
-        }
-	if temp.contains("R") {
-            print!("{}[2J", 27 as char);
-	    println!("Getting file...");
-            let encrypted_message = get_file(config.clone(),client.clone());
+}
+
+fn handle_receive_file(config:Arc<Mutex<Config>>, client:Arc<Client>, key:[u8;32], log:Arc<Mutex<Vec<String>>>){
+            match log.try_lock(){
+			    Ok(mut res) => res.push("Getting a file...".to_string()),
+			    Err(_) => ()
+			}
+            let encrypted_message = get_file(config.clone(),client.clone(), log.clone());
 	    if encrypted_message.filename.len() > 0 {
-                println!("Received {}", encrypted_message.clone().filename);
 		let encrypted_file = encrypted_message.clone().file.value;
 		let file_decrypted = encrypted_message.file.decrypt_aes_256_cbc(encrypted_file, &key).value;
 		let _ = fs::create_dir("shared");
 		let mut path = Path::new("shared/");
-		let mut file = File::create(path.join(encrypted_message.filename)).unwrap();
+		let mut file = File::create(path.join(encrypted_message.clone().filename)).unwrap();
 		file.write_all(&file_decrypted);
-		println!("Create file {}", path.display());
+		match log.try_lock(){
+			    Ok(mut res) => res.push(format!("Received, decrypted and saved file: {}", encrypted_message.filename)),
+			    Err(_) => ()
+			}
 	    }
-	    println!("Hit enter...");
-	    _ = stdin().read_line(&mut temp);
-        }
+}
+
+fn render_header(width: u16, heights: u16, startw:u16, starth:u16, label:&str, fill:char){
+    let mut stdout = stdout();
+    for w in startw..width{
+		    _ = stdout.queue(cursor::MoveTo(w as u16, starth as u16));
+		    _ = stdout.queue(Print(fill));
+    }
+    if (label.len()/2usize) < width as usize/2 {
+    let start_position = width/2 - (label.len()/2) as u16;
+	_ = stdout.queue(cursor::MoveTo(start_position, starth as u16));
+	 _ = stdout.queue(Print(label));
     }
 }
+
+fn render_text(width: u16, heights: u16, label:&str){
+    let mut stdout = stdout();
+    _ = stdout.queue(cursor::MoveTo(width, heights));
+    _ = stdout.queue(Print(label));
+}
+
+
+fn render_rectangle(width: u16, heights: u16, startw:u16, starth:u16){
+        let mut stdout = stdout();
+    	for w in startw..width{
+	    for h in starth..heights{
+		if (w == startw) && (h == starth){
+		    _ = stdout.queue(cursor::MoveTo(w as u16,h as u16));
+		    _ = stdout.queue(Print('┌'));
+		} else if( w == width-1) && (h == starth){
+       		    _ =  stdout.queue(cursor::MoveTo(w as u16,h as u16));
+		    _ = stdout.queue(Print('┐'));
+		} else if( w == startw) && (h == heights -1){
+       		    _ = stdout.queue(cursor::MoveTo(w as u16,h as u16));
+		    _ = stdout.queue(Print('└'));
+		} else if( w == width -1 ) && (h == heights -1){
+       		    _ = stdout.queue(cursor::MoveTo(w as u16,h as u16));
+		    _ = stdout.queue(Print('┘'));
+		} else if (h == starth) && (w !=startw) && (w< width -1){
+		    _ = stdout.queue(cursor::MoveTo(w as u16,h as u16));
+		    _ = stdout.queue(Print('─'));
+		} else if (h == heights -1 ) && (w !=startw) && (w< width -1){
+		    _ = stdout.queue(cursor::MoveTo(w as u16,h as u16));
+		    _ = stdout.queue(Print('─'));
+		} else if w == width -1{
+		    _ = stdout.queue(cursor::MoveTo(w as u16,h as u16));
+		    _ = stdout.queue(Print('│'));
+		} else if w == startw{
+		    _ = stdout.queue(cursor::MoveTo(w as u16,h as u16));
+		    _ = stdout.queue(Print('│'));
+		} else {
+		    _ = stdout.queue(cursor::MoveTo(w as u16,h as u16));
+		    _ = stdout.queue(Print('·'));		    
+		}
+	    }
+	}
+}
+
 
 fn clear_up_path(raw: String) -> String{
     let mut result = String::new();
@@ -75,7 +205,7 @@ fn clear_up_path(raw: String) -> String{
     result
 }
 
-fn watch_clipboard(config:Arc<Mutex<Config>>, client:Arc<Client>){
+fn watch_clipboard(config:Arc<Mutex<Config>>, client:Arc<Client>, log:Arc<Mutex<Vec<String>>>){
     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
     let key = aes_encryption::aes_encryption::get_key(config.clone().lock().unwrap().clone().Passkey);
     let mut prev_local_clipboard_hash = String::new();
@@ -88,26 +218,29 @@ fn watch_clipboard(config:Arc<Mutex<Config>>, client:Arc<Client>){
 	    Ok(tmp) => {
 		let mut flag = true;
 		let local_clip_message = Message::new(tmp.as_bytes().to_vec());
-		let new_remote_hash = get_text_hash(config.clone(), client.clone());
+		let new_remote_hash = get_text_hash(config.clone(), client.clone(), log.clone());
 		if new_remote_hash != prev_remote_clipboard_hash{
 		    let encrypted_text = get_text(config.clone(), client.clone());
 		    let decrypted_text = local_clip_message.decrypt_aes_256_cbc(encrypted_text.clone(), &key);
 		    temp_vec = encrypted_text.clone();
 		    match String::from_utf8(decrypted_text.value){
 			Ok(str) =>  ctx.set_contents(str).unwrap(),
-			Err(err) => eprintln!("failed to parse decrypted text, {}", err) 
+			Err(err) => match log.try_lock(){
+			    Ok(mut res) => res.push(format!("failed to parse decrypted text, {}", err)),
+			    Err(_) => ()
+			}
 		    }
 		    prev_remote_clipboard_hash = new_remote_hash;
 		    flag = false;
 		}
 		if flag && prev_local_clipboard_hash != local_clip_message.hash {
-		    post_text(config.clone(), local_clip_message.encrypt_aes_256_cbc(&key), local_clip_message.hash.clone().as_str(), client.clone());
+		    post_text(config.clone(), local_clip_message.encrypt_aes_256_cbc(&key), local_clip_message.hash.clone().as_str(), client.clone(), log.clone());
 		    prev_local_clipboard_hash = local_clip_message.hash.clone();
 		}
 
 	    },
 	    Err(_) => {
-		let new_remote_hash = get_text_hash(config.clone(), client.clone());
+		let new_remote_hash = get_text_hash(config.clone(), client.clone(), log.clone());
 		if new_remote_hash != prev_remote_clipboard_hash{
 		    let encrypted_text = get_text(config.clone(), client.clone());
 		    let local_clip_message = Message::new(Vec::new());
@@ -115,16 +248,19 @@ fn watch_clipboard(config:Arc<Mutex<Config>>, client:Arc<Client>){
 		    temp_vec = encrypted_text.clone();
 		    match String::from_utf8(decrypted_text.value){
 			Ok(str) =>  ctx.set_contents(str).unwrap(),
-			Err(err) => eprintln!("failed to parse decrypted text, {}", err) 
+			Err(err) => match log.try_lock(){
+				Ok(mut res) => res.push(format!("failed to parse decrypted text, {}", err)),
+				Err(_) => ()
+			}
+			}
 		    }
 		    prev_remote_clipboard_hash = new_remote_hash;
 	    } // eprintln!("Error getting clipboard: {}", err)
 	}	
 	}
-    }
-}
+  }
 
-fn get_text_hash(conf:Arc<Mutex<Config>>, client:Arc<Client>) -> String {
+fn get_text_hash(conf:Arc<Mutex<Config>>, client:Arc<Client>, log:Arc<Mutex<Vec<String>>>) -> String {
     let mut result = String::new();
     let response = client.get(form_url(conf.clone(), "/text_hash"))
         .header("TOKEN", conf.lock().unwrap().clone().Token)
@@ -134,10 +270,16 @@ fn get_text_hash(conf:Arc<Mutex<Config>>, client:Arc<Client>) -> String {
 	    let token_header = response.headers().get("HASH").map(|value| value.to_str().unwrap_or_default());
 	    match token_header{
 		Some(value) => {result.push_str(value)},
-		None => eprintln!("HASH header is absent from /test_hash endpoint")
+		None =>  match log.try_lock(){
+			    Ok(mut res) => res.push(format!("HASH header is absent from /test_hash endpoint")),
+			    Err(_) => ()
+			}
 	    }
 	},
-	Err(err) => eprintln!("Failed to send request to /text_hash endpoint because of: {}", err)
+	Err(err) =>  match log.try_lock(){
+			    Ok(mut res) => res.push(format!("Failed to send request to /text_hash endpoint because of: {}", err)),
+			    Err(_) => ()
+			}
     }
     result
 }
@@ -157,7 +299,7 @@ fn get_text(conf: Arc<Mutex<Config>>, client:Arc<Client>) -> Vec<u8> {
     result
 }
 
-fn post_text(conf: Arc<Mutex<Config>>, encrypted: Vec<u8>, hash: &str, client:Arc<Client>) {
+fn post_text(conf: Arc<Mutex<Config>>, encrypted: Vec<u8>, hash: &str, client:Arc<Client>, log:Arc<Mutex<Vec<String>>>) {
     let response = client.post(form_url(conf.clone(), "/text_post"))
         .header("TOKEN", conf.lock().unwrap().clone().Token)
         .header("HASH", hash)
@@ -165,11 +307,14 @@ fn post_text(conf: Arc<Mutex<Config>>, encrypted: Vec<u8>, hash: &str, client:Ar
         .send();
     match response{
 	Ok(_) => (),
-	Err(err) => eprintln!("Failed to post to /text_post because of {}", err)
-    }  
+	Err(err) =>  match log.try_lock(){
+			    Ok(mut res) => res.push(format!("Failed to post to /text_post because of {}", err)),
+			    Err(_) => ()
+			}
+    }
 }
 
-fn post_file(conf: Arc<Mutex<Config>>, encrypted: Vec<u8>, file_name:String, client:Arc<Client>) {
+fn post_file(conf: Arc<Mutex<Config>>, encrypted: Vec<u8>, file_name:String, client:Arc<Client>, log:Arc<Mutex<Vec<String>>>) {
     let response = client.post(form_url(conf.clone(), "/file_post"))
         .header("TOKEN", conf.lock().unwrap().clone().Token)
 	.header("FILENAME", &file_name)
@@ -177,11 +322,14 @@ fn post_file(conf: Arc<Mutex<Config>>, encrypted: Vec<u8>, file_name:String, cli
         .send();
     match response{
 	Ok(_) => (),
-	Err(err) => eprintln!("Failed to post to /file_post because of {}", err)
-    }  
+	Err(err) =>  match log.try_lock(){
+			    Ok(mut res) => res.push(format!("Failed to post to /file_post because of {}", err)),
+			    Err(_) => ()
+			}
+    }
 }
 
-fn get_file(conf: Arc<Mutex<Config>>, client:Arc<Client>) -> FileMessage {
+fn get_file(conf: Arc<Mutex<Config>>, client:Arc<Client>, log:Arc<Mutex<Vec<String>>>) -> FileMessage {
     let response = client.get(form_url(conf.clone(), "/file_get"))
         .header("TOKEN", conf.lock().unwrap().clone().Token)
         .body("")
@@ -196,7 +344,10 @@ fn get_file(conf: Arc<Mutex<Config>>, client:Arc<Client>) -> FileMessage {
 	},
 	Err(err) => {
 	    result_message = FileMessage::new("".to_string(), Vec::new());
-	    eprintln!("Failed to get to /file_get because of {}", err)
+	     match log.try_lock(){
+			    Ok(mut res) => res.push(format!("Failed to get to /file_get because of {}", err)),
+			    Err(_) => ()
+			}
 	}
     }
     result_message
@@ -218,13 +369,16 @@ fn main() {
         .expect("Should have been able to read the file");
     let conf:Config =  serde_json::from_str(&config).unwrap();
     let config_mutex = Arc::new(Mutex::new(conf.clone()));
+    let log:Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
+    let log2 = Arc::clone(&log);
     let config_mutex_copy = Arc::clone(&config_mutex);
     let http_client = Client::builder().danger_accept_invalid_certs(true).build().unwrap();
-    //let http_client = Client::new();
     let client_arc = Arc::new(http_client);
     let client_arc_copy = Arc::clone(&client_arc);
-    let scan_task = thread::spawn(move || {tui_scan(Arc::clone(&config_mutex.clone()), Arc::clone(&client_arc.clone()))}) ;
-    let _clip_task = thread::spawn(move ||{watch_clipboard(config_mutex_copy, client_arc_copy)});
-    scan_task.join().unwrap();
-    _clip_task.join().unwrap();
+    //let scan_task = thread::spawn(move || {tui_scan(Arc::clone(&config_mutex.clone()), Arc::clone(&client_arc.clone()))}) ;
+    //tx.send("Starting dojo\n".to_string()).unwrap_or_else(|_|{});
+    let _clip_task = thread::spawn(move ||{watch_clipboard(config_mutex_copy, client_arc_copy, log)});
+    let _tui_task = thread::spawn(move || {render_ui(Arc::clone(&config_mutex.clone()), Arc::clone(&client_arc.clone()), log2)});
+    //_clip_task.join().unwrap();
+    _tui_task.join().unwrap();
 }
